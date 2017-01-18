f74d0dcdfe1b9a344cdbed0783b95ef51c688a9b
sejgit <sejgit@github.com>
2017-01-18 13:50:13 -0500

    up

---
+++ elpa/sunrise-x-loop-3.423/sunrise-x-loop-autoloads.el
@@ -0,0 +1,18 @@
+;;; sunrise-x-loop-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "sunrise-x-loop" "sunrise-x-loop.el" (22613
+;;;;;;  51122 0 0))
+;;; Generated autoloads from sunrise-x-loop.el
+ (eval-after-load 'sunrise-commander '(sr-extend-with 'sunrise-x-loop))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sunrise-x-loop-autoloads.el ends here
