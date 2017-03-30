94a1b626458c69156e73b40d8348660a4a185bb8
sejgit <sejgit@github.com>
2017-03-30 10:21:36 -0400

    up

---
+++ elpa/dired-launch-20170328.809/dired-launch-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-launch" "20170328.809" "Use dired as a launcher" 'nil :url "https://github.com/thomp/dired-launch" :keywords '("dired" "launch"))
---
+++ elpa/pyenv-mode-auto-20160122.2341/pyenv-mode-auto-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pyenv-mode-auto" "20160122.2341" "Automatically activates pyenv version if .python-version file exists." '((pyenv-mode "0.1.0") (s "1.11.0") (f "0.17.0")) :url "https://github.com/ssbb/pyenv-mode-auto" :keywords '("python" "pyenv"))
