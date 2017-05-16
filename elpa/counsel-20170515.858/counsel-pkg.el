4d476f0655684ef3fad17818fb019dac7f35badb
sejgit <sejgit@github.com>
2017-05-16 13:40:58 -0400

    update

--- elpa/counsel-20170515.858/counsel-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel" "20170515.858" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "38ecfe7d21730f56b9e0c71b26a5aab8f03d922f" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
--- elpa/rpn-calc-20170512.235/rpn-calc-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; rpn-calc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "rpn-calc" "rpn-calc.el" (22806 18361 0 0))
-;;; Generated autoloads from rpn-calc.el
-
-(autoload 'rpn-calc "rpn-calc" "\
-quick RPN calculator for hackers
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; rpn-calc-autoloads.el ends here
