6b1a3c957175a7b3e966c156b502436e458d545a
sejgit <sejgit@github.com>
2017-05-18 13:39:43 -0400

    simplify smex

--- init.d/init-smex.el
+++ init.d/init-smex.el
@@ -5,15 +5,16 @@
 ;; 2016 12 16
 ;; 2017 01 07 switch from req-package to use-package
 ;; 2017 04 04 remove ensure went global ; defer not required for mode,bind,int
+;; 2017 05 18 simplify
 
 ;;; Code:
 
 (use-package smex
-  :defer 5
   :bind (("M-x" . smex)
 	 ("M-X" . smex-major-mode-commands))
-  :config (progn (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-		 (smex-initialize)))
+  :config
+  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
+  (smex-initialize))
 
 (provide 'init-smex)
 ;;; init-smex ends here
