f902455465f1ef7d0e1e36e5ae8b01b36ee044a5
sejgit <sejgit@github.com>
2017-06-04 20:21:10 -0400

    projectile mods

--- init.d/init-projectile.el
+++ init.d/init-projectile.el
@@ -2,24 +2,17 @@
 
 ;;; Commentary:
 ;; 2017 05 14 SeJ init from purcell/.emacs.d
-
+;; 2017 06 01 simplified & added helm-projectile
 
 ;;; Code:
 
 (use-package projectile
   :config
-  (add-hook 'after-init-hook 'projectile-global-mode)
-  ;; The following code means you get a menu if you hit "C-c p" and wait
-  (setq-default
-   projectile-mode-line
-   '(:eval
-     (if (file-remote-p default-directory)
-	 " Pr"
-       (format " Pr[%s]" (projectile-project-name))))))
+  (add-hook 'after-init-hook 'projectile-global-mode))
 
-(use-package guide-key
+(use-package helm-projectile
   :config
-  (add-to-list 'guide-key/guide-key-sequence "C-c p"))
+  (helm-projectile-on))
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
