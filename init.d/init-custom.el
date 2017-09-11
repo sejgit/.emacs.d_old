147427584fe2c67368f04f64e3f3dc079e9eaa77
sejgit <sejgit@github.com>
2017-09-11 12:24:31 -0400

    add abbrev_defs to .gitignore

--- init.d/init-custom.el
+++ init.d/init-custom.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
  '(midnight-mode t)
  '(package-selected-packages
    (quote
