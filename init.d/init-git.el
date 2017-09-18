3088d910e140f81444c6b66f58f963d5b37e1b0a
sejgit <sejgit@github.com>
2017-09-18 07:05:35 -0400

    add full screen for magit status and return upon quit to init-git.el

--- init.d/init-git.el
+++ init.d/init-git.el
@@ -8,6 +8,7 @@
 ;; 2017 06 12 add font-awesome git icon
 ;; 2017 08 29 map to sej-mode-map & documentation & defer/ensure
 ;; 2017 08 30 deleted a few packages & documented the rest
+;; 2017 09 18 add full screen for magit-status and return to previous on quit
 
 ;;; Code:
 
@@ -102,6 +103,21 @@
     (concat [#xF1D3] ":" (substring-no-properties str 4))))
 
 (advice-add #'vc-git-mode-line-string :around #'my-vc-git-mode-line-string)
+
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 (provide 'init-git)
 ;;;init-git.el ends here
