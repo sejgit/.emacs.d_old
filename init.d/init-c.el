6d668ac9564de44d01567d1f6cdc9f41dea9f947
sejgit <sejgit@github.com>
2018-08-09 19:59:12 -0400

    cc-mode & Cedet set up for c & arduino

--- init.d/init-c.el
+++ init.d/init-c.el
@@ -5,11 +5,33 @@
 
 ;;; Log
 ;; 2017 08 25 init SeJ
+;; 2018 08 09 udates for Cedet
 
 ;;; Code:
 
 
 (require 'cc-mode)
+
+;; ;; Load CEDET.
+;; ;; See cedet/common/cedet.info for configuration details.
+;; ;; IMPORTANT: For Emacs >= 23.2, you must place this *before* any
+;; ;; CEDET component (including EIEIO) gets activated by another
+;; ;; package (Gnus, auth-source, ...).
+;; (load-file "~/.emacs.d/vendor/cedet/cedet-devel-load.el")
+
+;; ;; Add further minor-modes to be enabled by semantic-mode.
+;; ;; See doc-string of `semantic-default-submodes' for other things
+;; ;; you can use here.
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+;; ;; Enable Semantic
+(semantic-mode 1)
+
+;; ;; Enable EDE (Project Management) features
+;;(global-ede-mode 1)
+
 
 ;; Flycheck supports C, so we switch it on
 (add-hook 'c-mode-common-hook #'flycheck-mode)
