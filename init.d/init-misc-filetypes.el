7365535a96a2f407af37f341c1b024292d0d22f7
sejgit <sejgit@github.com>
2018-09-28 21:39:55 -0400

    working js mode & helpers from ohai

--- init.d/init-misc-filetypes.el
+++ init.d/init-misc-filetypes.el
@@ -93,9 +93,50 @@
 (use-package js2-mode
   :ensure t
   :mode (("\\.js\\'" . js2-mode)
-	 ("\\.json\\'" . js2-mode)
-	 ("\\manifest.webapp\\'" . js2-mode )
-	 ("\\.tern-project\\'" . js2-mode)))
+	       ("\\.json\\'" . js2-mode)
+	       ("\\.js$\\'" . js2-mode )
+	       ("\\.es6\\'" . js2-mode )
+	       ("\\.ejs\\'" . js2-mode )
+	       ("\\manifest.webapp\\'" . js2-mode )
+	       ("\\.tern-project\\'" . js2-mode))
+  :interpreter "node"
+  :commands js2-mode
+  :config
+  ;; Leverage js2-mode to get some refactoring support through js2-refactor.
+  (use-package js2-refactor
+    :ensure t
+    :commands (js2r-add-keybindings-with-prefix)
+    :init
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)
+    (js2r-add-keybindings-with-prefix "C-c C-m"))
+  ;; Configure js2-mode good.
+  (setq-default
+   js2-mode-indent-ignore-first-tab t
+   js2-strict-inconsistent-return-warning nil
+   js2-global-externs
+   '("module" "require" "__dirname" "process" "console" "JSON" "$" "_"))
+  ;; js2-show-parse-errors nil
+  ;; js2-strict-var-hides-function-arg-warning nil
+  ;; js2-strict-missing-semi-warning nil
+  ;; js2-strict-trailing-comma-warning nil
+  ;; js2-strict-cond-assign-warning nil
+  ;; js2-strict-var-redeclaration-warning nil
+  )
+
+;; Use Tern for smarter JS.
+(use-package tern
+  :ensure t
+  :commands tern-mode
+  :config
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+  ;; Locate the Tern binary by querying the system search path, which
+  ;; should now include the local npm prefix.
+  (setq tern-command (list (or (ohai/resolve-exec "tern") "tern")))
+  ;; Setup Tern as an autocomplete source.
+  (with-eval-after-load "company"
+    (use-package company-tern
+      :config
+      (add-to-list 'company-backends 'company-tern))))
 
 (use-package web-mode
   :ensure t
