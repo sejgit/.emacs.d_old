8fe7deebece68c25744b1971e802306ad89e1655
sejgit <sejgit@github.com>
2018-10-15 19:59:25 -0400

    add js to init-languages

--- init.d/init-languages.el
+++ init.d/init-languages.el
@@ -15,8 +15,7 @@
 ;; 2018 06 06 add company-jedi (not sure of interactions)
 ;; 2018 10 09 some changes to work with language-server-protocall
 ;; 2018 10 10 changed to init-languages.el to hold all lsp type stuff
-;; 2018 10 15 lsp added for c modes, html modes, css, python, bash/sh
-;;            TODO: add for java, js
+;; 2018 10 15 lsp added for c modes, html modes, css, python, bash/sh, java, js
 
 
 ;;; Code:
@@ -277,6 +276,80 @@
 ;; (use-package py-autopep8
 ;;   :ensure t
 ;;   :hook (python-mode . py-autopep8-enable-on-save))
+
+
+;;
+;; javascript
+;;
+
+(use-package js2-mode
+  :ensure t
+  :mode (("\\.js\\'" . js2-mode)
+	       ("\\.json\\'" . js2-mode)
+	       ("\\.js$\\'" . js2-mode )
+	       ("\\.es6\\'" . js2-mode )
+	       ("\\.ejs\\'" . js2-mode )
+	       ("\\manifest.webapp\\'" . js2-mode )
+	       ("\\.tern-project\\'" . js2-mode))
+  :interpreter "node"
+  :commands js2-mode
+  :config
+  ;; Leverage js2-mode to get some refactoring support through js2-refactor.
+  (use-package js2-refactor
+    :ensure t
+    :commands (js2r-add-keybindings-with-prefix)
+    :init
+    (add-hook 'js2-mode-hook #'js2-refactor-mode)
+    (js2r-add-keybindings-with-prefix "C-c C-m"))
+  ;; Configure js2-mode good.
+  (setq-default
+   js2-mode-indent-ignore-first-tab t
+   js2-strict-inconsistent-return-warning nil
+   js2-global-externs
+   '("module" "require" "__dirname" "process" "console" "JSON" "$" "_"))
+  ;; js2-show-parse-errors nil
+  ;; js2-strict-var-hides-function-arg-warning nil
+  ;; js2-strict-missing-semi-warning nil
+  ;; js2-strict-trailing-comma-warning nil
+  ;; js2-strict-cond-assign-warning nil
+  ;; js2-strict-var-redeclaration-warning nil
+  )
+
+;; Use Tern for smarter JS.
+(use-package tern
+  :ensure t
+  :commands tern-mode
+  :config
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+  ;; Locate the Tern binary by querying the system search path, which
+  ;; should now include the local npm prefix.
+  (setq tern-command (list (or (ohai/resolve-exec "tern") "tern")))
+  ;; Setup Tern as an autocomplete source.
+  (with-eval-after-load "company"
+    (use-package company-tern
+      :config
+      (add-to-list 'company-backends 'company-tern))))
+
+;; ;; you also need lsp-javascript-typescript installed and on your PATH
+;; npm -i -g javascript-typescript-langserver
+(use-package lsp-javascript-typescript
+  :ensure t
+  :init
+  (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
+  (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable) ;; for typescript support
+  (add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
+  (add-hook 'rjsx-mode #'lsp-javascript-typescript-enable) ;; for rjsx-mode support
+
+  (defun my-company-transformer (candidates)
+    (let ((completion-ignore-case t))
+      (all-completions (company-grab-symbol) candidates)))
+
+  (defun my-js-hook nil
+    (make-local-variable 'company-transformers)
+    (push 'my-company-transformer company-transformers))
+
+  (add-hook 'js-mode-hook 'my-js-hook))
+
 
 (provide 'init-languages)
 ;;; init-languages.el ends here
